91b746f07cb5ddbe2d1226efdd7a46862a66a95d
Ashish Dixit <ashishdixit815@gmail.com>
2020-04-08 13:37:05 +0530

    Fragment Image has been opened

--- app/src/main/java/com/ashish/ca/GallaryApp/FragmentOne.java
+++ app/src/main/java/com/ashish/ca/GallaryApp/FragmentOne.java
@@ -1,5 +1,10 @@
 package com.ashish.ca.GallaryApp;
 
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +12,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.ashish.ca.R;
+
+import static android.content.Context.MODE_PRIVATE;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,6 +29,9 @@ public class FragmentOne extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    SharedPreferences sp;
+    ImageView iv;
+    Bitmap bitmap;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -50,6 +62,7 @@ public class FragmentOne extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -60,6 +73,21 @@ public class FragmentOne extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_one, container, false);
+        View root=inflater.inflate(R.layout.fragment_one, container, false);
+        sp = this.getActivity().getSharedPreferences("student", MODE_PRIVATE);
+        String savedPicturePath = sp.getString("imagepath","null");
+        if(!savedPicturePath.equals("null")){
+
+            setImageInViewPager(savedPicturePath,root);
+        }
+
+        return root;
+    }
+    public void setImageInViewPager(String savedPicturePath,View root) {
+
+        bitmap = BitmapFactory.decodeFile(savedPicturePath);
+        Drawable d = new BitmapDrawable(getResources(),bitmap);
+        iv = root.findViewById(R.id.imageView);
+        iv.setImageBitmap(bitmap);
     }
 }
--- app/src/main/java/com/ashish/ca/GallaryApp/Gallary_Main.java
+++ app/src/main/java/com/ashish/ca/GallaryApp/Gallary_Main.java
@@ -4,22 +4,32 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.RelativeLayout;
 import android.widget.TableLayout;
 
 import com.ashish.ca.R;
 
 public class Gallary_Main extends AppCompatActivity {
     ViewPager viewPager;
+    SharedPreferences sp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallary__main);
         viewPager=findViewById(R.id.viewPager);
-
+        sp=getSharedPreferences("student",MODE_PRIVATE);
         ViewPagerAdapter adapter=new ViewPagerAdapter(getSupportFragmentManager());
         adapter.add(new FragmentOne(),"One");
         adapter.add(new FragmentTwo(),"Two");
@@ -44,12 +54,34 @@ public class Gallary_Main extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch(item.getItemId()) {
             case R.id.select_from_gallary:
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
 
+                startActivityForResult(i, 101);
                 return true;
 
 
             default:
             return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    public void startActivityForResult(Intent data, int requestCode,int resultCode) {
+        if (requestCode == 101 && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            sp.edit().putString("imagepath", picturePath);
+            cursor.close();
+
+
         }
     }
 }
